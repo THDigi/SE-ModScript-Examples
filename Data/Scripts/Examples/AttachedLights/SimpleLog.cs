a7135dd6d004e3e767f9583539051a299c79eec2
Digi <hunter_digital_sl@yahoo.com>
2020-04-22 17:37:39 +0300

    AttachedLights: Minor logger tweaks

--- Data/Scripts/Examples/AttachedLights/SimpleLog.cs
+++ Data/Scripts/Examples/AttachedLights/SimpleLog.cs
@@ -9,18 +9,29 @@ namespace Digi.AttachedLights
     {
         public static void Error(object caller, Exception e)
         {
-            MyLog.Default.WriteLineAndConsole($"{e.Message}\n{e.StackTrace}");
+            MyLog.Default.WriteLineAndConsole($"ERROR {caller.GetType().FullName}: {e.ToString()}");
+            MyLog.Default.Flush();
 
             if(MyAPIGateway.Session?.Player != null)
-                MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {caller.GetType().FullName}: {e.Message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+                MyAPIGateway.Utilities.ShowNotification($"[ERROR: {caller.GetType().FullName}: {e.Message}] | Send SpaceEngineers.Log to mod author", 10000, MyFontEnum.Red);
         }
 
         public static void Error(object caller, string message)
         {
             MyLog.Default.WriteLineAndConsole($"ERROR {caller.GetType().FullName}: {message}");
+            MyLog.Default.Flush();
 
             if(MyAPIGateway.Session?.Player != null)
-                MyAPIGateway.Utilities.ShowNotification($"[ ERROR: {caller.GetType().FullName}: {message} | Send SpaceEngineers.Log to mod author ]", 10000, MyFontEnum.Red);
+                MyAPIGateway.Utilities.ShowNotification($"[ERROR: {caller.GetType().FullName}: {message}] | Send SpaceEngineers.Log to mod author", 10000, MyFontEnum.Red);
+        }
+
+        public static void Info(object caller, string message, bool notify = false, int notifyTime = 5000)
+        {
+            MyLog.Default.WriteLineAndConsole($"WARNING {caller.GetType().FullName}: {message}");
+            MyLog.Default.Flush();
+
+            if(notify)
+                MyAPIGateway.Utilities?.ShowNotification($"[WARNING {caller.GetType().FullName}: {message}]", notifyTime, MyFontEnum.Green);
         }
     }
 }
