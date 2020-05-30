3076a3e69af4d5ff5106eb3a003b9ec5bc3dbd1c
Digi <hunter_digital_sl@yahoo.com>
2020-05-30 23:18:15 +0300

    Added TSS example

---
+++ Data/Scripts/Examples/Example_TSS.cs
@@ -0,0 +1,43 @@
+﻿using Sandbox.Game.GameSystems.TextSurfaceScripts;
+using Sandbox.ModAPI;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+namespace Digi.Examples
+{
+    [MyTextSurfaceScript("InternalNameHere", "Public Name Here")]
+    public class SomeClassName : MyTSSCommon
+    {
+        public override ScriptUpdate NeedsUpdate => ScriptUpdate.Update10; // frequency that Run() is called.
+
+        public SomeClassName(IMyTextSurface surface, IMyCubeBlock block, Vector2 size) : base(surface, block, size)
+        {
+            // initialization
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose(); // do not remove
+
+            // when script is removed, clean up stuff
+        }
+
+        public override void Run()
+        {
+            base.Run(); // do not remove
+
+            using(var frame = Surface.DrawFrame())
+            {
+                Vector2 screenSize = Surface.SurfaceSize;
+                Vector2 screenCorner = (Surface.TextureSize - screenSize) * 0.5f;
+
+                var text = MySprite.CreateText("Hi!", "White", Surface.ScriptForegroundColor, 1f, TextAlignment.LEFT);
+                text.Position = screenCorner + new Vector2(16, 16); // 16px from topleft corner of the visible surface
+                frame.Add(text);
+
+                // add more sprites and stuff
+            }
+        }
+    }
+}
