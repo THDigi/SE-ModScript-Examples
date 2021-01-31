94168101386c27fddb99ce1f8d17d1a85def5e98
Digi <hunter_digital_sl@yahoo.com>
2021-01-31 22:57:06 +0200

    Fixed solo DS testing not being set up for spaces in steam path.

--- Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
+++ Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
@@ -1,9 +1,13 @@
+rem Edit these 3 lines only and do not add quotes!
+
 set WORKSHOP_ID=YourWorkshopIdHere
 set MOD_SCRIPT_PATH=YourScriptFolderNameHere
-set STEAM_PATH=C:\Steam
+set STEAM_PATH=C:\Program Files (x86)\Steam
 
-set CLIENT_PATH=%STEAM_PATH%\steamapps\workshop\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\
-set DS_PATH=%APPDATA%\SpaceEngineersDedicated\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\
+rem Do not edit below this.
+
+set CLIENT_PATH="%STEAM_PATH%\steamapps\workshop\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\"
+set DS_PATH="%APPDATA%\SpaceEngineersDedicated\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\"
 
 rmdir %DS_PATH% /S /Q
 rmdir %CLIENT_PATH% /S /Q
