adb8c08bc73ab583a7d1021d7bef6bb27a81555b
Digi <hunter_digital_sl@yahoo.com>
2021-02-10 19:48:48 +0200

    Updated solo DS testing with full-mod-copy batch and integrated instructions

--- Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
+++ Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
@@ -1,18 +1,31 @@
-rem Edit these 3 lines only and do not add quotes!
+@echo off
+rem	Testing mods in DS by yourself can be done without the need to re-publish every time.
+rem	You can simply update the files that are on your machine!
+rem	This will only work for you, anyone else joining the server will of course download the mod from the workshop.
 
-set WORKSHOP_ID=YourWorkshopIdHere
-set MOD_SCRIPT_PATH=YourScriptFolderNameHere
+rem	To use:
+rem	1. Copy this .bat file in the ROOT folder of your local mod (e.g. SpaceEngineers/Mods/YourLocalMod/<HERE>)
+
+rem	2. Edit this variable if applicable (do not add quotes or end with backslash).
 set STEAM_PATH=C:\Program Files (x86)\Steam
 
-rem Do not edit below this.
+rem	3. Edit this with your mod's workshop id.
+set WORKSHOP_ID=NumberHere
 
-set CLIENT_PATH="%STEAM_PATH%\steamapps\workshop\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\"
-set DS_PATH="%APPDATA%\SpaceEngineersDedicated\content\244850\%WORKSHOP_ID%\Data\Scripts\%MOD_SCRIPT_PATH%\"
+rem	Now you can run it every time you want to update the mod on DS and client.
 
-rmdir %DS_PATH% /S /Q
-rmdir %CLIENT_PATH% /S /Q
 
-robocopy.exe .\ %DS_PATH% *.cs /xd "bin" "obj" ".vs" /S
-robocopy.exe .\ %CLIENT_PATH% *.cs /xd "bin" "obj" ".vs" /S
+
+rem 	Don't edit the below unless you really need different paths.
+rem	NOTE: don't add quotes and don't end with a backslash!
+
+set CLIENT_PATH=%STEAM_PATH%\steamapps\workshop\content\244850\%WORKSHOP_ID%
+set DS_PATH=%APPDATA%\SpaceEngineersDedicated\content\244850\%WORKSHOP_ID%
+
+rmdir "%CLIENT_PATH%" /S /Q
+rmdir "%DS_PATH%" /S /Q
+
+robocopy.exe .\ "%DS_PATH%"  *.* /S /xd .git bin obj .vs ignored /xf *.lnk *.git* *.bat *.zip *.7z *.blend* *.md *.log *.sln *.csproj *.csproj.user *.ruleset
+robocopy.exe "%DS_PATH%" "%CLIENT_PATH%" *.* /S
 
 pause
