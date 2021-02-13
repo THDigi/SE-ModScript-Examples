d80822e34a499d9821dcabe6dd91f468299f20e8
Digi <hunter_digital_sl@yahoo.com>
2021-02-13 15:52:39 +0200

    Small improvements to FastSoloDSTesting

--- Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
+++ Data/Scripts/Examples/Fast Solo DS Testing/copy to DS and client.bat
@@ -4,7 +4,7 @@ rem	You can simply update the files that are on your machine!
 rem	This will only work for you, anyone else joining the server will of course download the mod from the workshop.
 
 rem	To use:
-rem	1. Copy this .bat file in the ROOT folder of your local mod (e.g. SpaceEngineers/Mods/YourLocalMod/<HERE>)
+rem	1. Copy this .bat file in the ROOT folder of your local mod (e.g. %appdata%/SpaceEngineers/Mods/YourLocalMod/<HERE>)
 
 rem	2. Edit this variable if applicable (do not add quotes or end with backslash).
 set STEAM_PATH=C:\Program Files (x86)\Steam
@@ -25,7 +25,7 @@ set DS_PATH=%APPDATA%\SpaceEngineersDedicated\content\244850\%WORKSHOP_ID%
 rmdir "%CLIENT_PATH%" /S /Q
 rmdir "%DS_PATH%" /S /Q
 
-robocopy.exe .\ "%DS_PATH%"  *.* /S /xd .git bin obj .vs ignored /xf *.lnk *.git* *.bat *.zip *.7z *.blend* *.md *.log *.sln *.csproj *.csproj.user *.ruleset
+robocopy.exe .\ "%DS_PATH%"  *.* /S /xd .git bin obj .vs ignored /xf *.lnk *.git* *.bat *.zip *.7z *.blend* *.md *.log *.sln *.csproj *.csproj.user *.ruleset modinfo.sbmi
 robocopy.exe "%DS_PATH%" "%CLIENT_PATH%" *.* /S
 
 pause
