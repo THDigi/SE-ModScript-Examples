da693cd175ceb37b3214b11897c657549fe06777
Digi <hunter_digital_sl@yahoo.com>
2021-03-06 07:45:02 +0200

    Improved comments on Session+GameLogic examples

--- Data/Scripts/Examples/BasicExample_GameLogicAndSession/GameLogic.cs
+++ Data/Scripts/Examples/BasicExample_GameLogicAndSession/GameLogic.cs
@@ -8,10 +8,11 @@ namespace Digi.Examples
 {
     // This object gets attached to entities depending on their type and optionally subtype aswell.
     // The 2nd arg, "false", is for entity-attached update if set to true which is not recommended, see for more info: https://forum.keenswh.com/threads/modapi-changes-jan-26.7392280/
+    // Remove any method that you don't need, they're only there to show what you can use, and also remove comments you've read as they're only for example purposes and don't make sense in a final mod.
     [MyEntityComponentDescriptor(typeof(MyObjectBuilder_BatteryBlock), false, "SubTypeIdHere", "more if needed...")]
     public class Example_GameLogic : MyGameLogicComponent
     {
-        private IMyCubeBlock block; // storing the entity as a block reference to avoid re-casting it every time it's needed
+        private IMyCubeBlock block; // storing the entity as a block reference to avoid re-casting it every time it's needed, this is the lowest type a block entity can be.
 
         public override void Init(MyObjectBuilder_EntityBase objectBuilder)
         {
@@ -24,38 +25,41 @@ namespace Digi.Examples
 
         public override void UpdateOnceBeforeFrame()
         {
-            // first update of the block, remove if not needed
+            // first update, remove if not needed
 
             if(block.CubeGrid?.Physics == null) // ignore projected and other non-physical grids
                 return;
 
             // do stuff
 
-            // access stuff from session via Example_Session.Instance. ...
+            // you can access stuff from session via Example_Session.Instance.[...]
 
             NeedsUpdate = MyEntityUpdateEnum.EACH_FRAME | MyEntityUpdateEnum.EACH_100TH_FRAME; // allow UpdateAfterSimulation() and UpdateAfterSimulation100() to execute, remove if not needed
         }
 
-        public override void UpdateAfterSimulation()
+        public override void MarkForClose()
         {
-            // executed 60 times a second after physics simulation, unless game is paused
+            // called when entity is about to be removed for whatever reason (block destroyed, entity deleted, ship despawn because of sync range, etc)
         }
 
         public override void UpdateBeforeSimulation()
         {
-            // NOTE: this requires the proper NeedsUpdate flag.
+            // executed 60 times a second after physics simulation, unless game is paused.
+            // triggered only if NeedsUpdate contains MyEntityUpdateEnum.EACH_FRAME.
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            // executed 60 times a second before physics simulation, unless game is paused.
+            // triggered only if NeedsUpdate contains MyEntityUpdateEnum.EACH_FRAME.
         }
 
         public override void UpdateAfterSimulation100()
         {
-            // executed APPROXIMATELY every 100 ticks (60 ticks in a second), unless game is paused.
-            // Why approximately? Explained at the "Important information" in: https://forum.keenswh.com/threads/pb-scripting-guide-how-to-use-self-updating.7398267/
-            // there's also a 10 tick variant.
-        }
-
-        public override void Close()
-        {
-            // called when block is removed for whatever reason (incl ship despawn, etc).
+            // executed approximately every 100 ticks (~1.66s), unless game is paused.
+            // why approximately? Explained at the "Important information" in: https://forum.keenswh.com/threads/pb-scripting-guide-how-to-use-self-updating.7398267/
+            // there's also a 10-tick variant.
+            // triggered only if NeedsUpdate contains MyEntityUpdateEnum.EACH_100TH_FRAME, same for UpdateBeforeSimulation100().
         }
     }
 }
--- Data/Scripts/Examples/BasicExample_GameLogicAndSession/Session.cs
+++ Data/Scripts/Examples/BasicExample_GameLogicAndSession/Session.cs
@@ -1,28 +1,40 @@
 ﻿using System;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 using VRage.Game;
 using VRage.Game.Components;
 using VRage.Utils;
+using BlendTypeEnum = VRageRender.MyBillboard.BlendTypeEnum; // required for MyTransparentGeometry/MySimpleObjectDraw to be able to set blend type.
 
 namespace Digi.Examples
 {
     // This object is always present, from the world load to world unload.
     // NOTE: all clients and server run mod scripts, keep that in mind.
+    // NOTE: this and gamelogic comp's update methods run on the main game thread, don't do too much in a tick or you'll lower sim speed.
+    // NOTE: also mind allocations, avoid realtime allocations, re-use collections/ref-objects (except value types like structs, integers, etc).
+    //
     // The MyUpdateOrder arg determines what update overrides are actually called.
     // Remove any method that you don't need, none of them are required, they're only there to show what you can use.
     // Also remove all comments you've read to avoid the overload of comments that is this file.
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation | MyUpdateOrder.AfterSimulation)]
     public class Example_Session : MySessionComponentBase
     {
-        public static Example_Session Instance; // the only way to access session comp from other classes and the only accepted static.
+        public static Example_Session Instance; // the only way to access session comp from other classes and the only accepted static field.
 
         public override void LoadData()
         {
             // amogst the earliest execution points, but not everything is available at this point.
 
-            // main entry point: MyAPIGateway
-            // entry point for reading/editing definitions: MyDefinitionManager.Static
-            // these can be used anywhere as they're types not fields.
+            // These can be used anywhere, not just in this method/class:
+            // MyAPIGateway. - main entry point for the API
+            // MyDefinitionManager.Static. - reading/editing definitions
+            // MyGamePruningStructure. - fast way of finding entities in an area
+            // MyTransparentGeometry. and MySimpleObjectDraw. - to draw sprites (from TransparentMaterials.sbc) in world (they usually live a single tick)
+            // MyVisualScriptLogicProvider. - mainly designed for VST but has its uses, use as a last resort.
+            // System.Diagnostics.Stopwatch - for measuring code execution time.
+            // ...and many more things, ask in #programming-modding in keen's discord for what you want to do to be pointed at the available things to use.
 
             Instance = this;
         }
@@ -81,6 +93,13 @@ namespace Digi.Examples
         public override void SaveData()
         {
             // executed AFTER world was saved
+        }
+
+        public override MyObjectBuilder_SessionComponent GetObjectBuilder()
+        {
+            // executed during world save, most likely before entities.
+
+            return base.GetObjectBuilder(); // leave as-is.
         }
 
         public override void UpdatingStopped()
