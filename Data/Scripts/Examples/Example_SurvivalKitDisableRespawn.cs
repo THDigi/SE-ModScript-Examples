60abe13626f91ee3245487140f33ef6ba25931f8
Digi <hunter_digital_sl@yahoo.com>
2022-06-16 16:07:02 +0300

    Added disabling respawn of survival kit

---
+++ Data/Scripts/Examples/Example_SurvivalKitDisableRespawn.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.ObjectBuilders;
+
+namespace Digi.Examples
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_SurvivalKit), false, "subtype here", "and more...", "etc...")]
+    public class Example_SurvivalKitDisableRespawn : MyGameLogicComponent
+    {
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            Entity.Components.Remove<MyEntityRespawnComponentBase>();
+        }
+    }
+}
