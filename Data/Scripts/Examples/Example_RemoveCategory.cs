42661f2eff7f94f2da11b318b30c4fab0e469915
Digi <hunter_digital_sl@yahoo.com>
2022-06-21 06:22:50 +0300

    Added remove category example

---
+++ Data/Scripts/Examples/Example_RemoveCategory.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox.Definitions;
+using VRage.Game.Components;
+
+namespace Digi.Experiments
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Example_RemoveCategory : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            RemoveCategory("LargeBlocks");
+            // can repeat the above line to remove more
+        }
+
+        void RemoveCategory(string categoryName)
+        {
+            MyGuiBlockCategoryDefinition categoryDef;
+            if(MyDefinitionManager.Static.GetCategories().TryGetValue(categoryName, out categoryDef))
+            {
+                categoryDef.ItemIds.Clear();
+
+                categoryDef.IsBlockCategory = true;
+                categoryDef.IsAnimationCategory = false;
+                categoryDef.IsShipCategory = false;
+                categoryDef.IsToolCategory = false;
+            }
+        }
+
+        protected override void UnloadData()
+        {
+        }
+    }
+}
