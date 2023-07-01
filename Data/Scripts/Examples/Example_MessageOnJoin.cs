139351d55c10fd10f0c20c317d556b237b3b994c
Digi <844339+THDigi@users.noreply.github.com>
2023-07-01 18:16:16 +0300

    Added MessageOnJoin example

---
+++ Data/Scripts/Examples/Example_MessageOnJoin.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox.ModAPI;
+using VRage.Game.Components;
+
+namespace Digi.Examples
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    public class Example_MessageOnJoin : MySessionComponentBase
+    {
+        const string From = "Nigerian Prince";
+        const string Message = "Hello, I give you lots of money if you give me few money.";
+
+        bool SeenMessage = false;
+
+        public override void LoadData()
+        {
+            if(MyAPIGateway.Session.IsServer && MyAPIGateway.Utilities.IsDedicated) // DS side does not need this
+                return;
+
+            SetUpdateOrder(MyUpdateOrder.AfterSimulation);
+        }
+
+        public override void UpdateAfterSimulation()
+        {
+            if(!SeenMessage && MyAPIGateway.Session?.Player?.Character != null)
+            {
+                SeenMessage = true;
+                MyAPIGateway.Utilities.ShowMessage(From, Message);
+
+                // required delayed like this because it modifies the list that iterates components to trigger this update method, causing list modified exception.
+                MyAPIGateway.Utilities.InvokeOnGameThread(() => SetUpdateOrder(MyUpdateOrder.NoUpdate));
+            }
+        }
+    }
+}
